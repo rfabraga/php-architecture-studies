064bd8cc94e569ceac3d0c0a979cea77a6c1e7df
rfabraga <rfl.braganholli@gmail.com>
2021-06-24 22:11:41 -0300

    Adding named constructor to Student.

--- src/Student.php
+++ src/Student.php
@@ -8,9 +8,15 @@ class Student
     private string $name;
     private Email $email;
     private array $phones;
+
+    public static function withCpfNameAndEmail(string $cpf, string $name, string $email)
+    {
+        return new Student(new CPF($cpf), $name, new Email($email));
+    }
     
-    public function addPhone(string $ddd, string $number): void
+    public function addPhone(string $ddd, string $number): self
     {
         $this->phones[] = new Phone($ddd, $number);
+        return $this;
     }
 }
